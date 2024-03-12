d995c39987209a17558eb218741933b9b80f0f5d
Шлыков Андрей Владимирович (4075486) <avshlykov@inno.tech>
2024-03-12 14:07:05 +0300

    clean

--- reserv/run.bat
+++
@@ -1,5 +0,0 @@
-set DATABASE_URL_TASK5=jdbc:postgresql://localhost:5432/hwork5
-set USERNAME_TASK5=postgres
-set PASSWORD_TASK5=1Q2w3e4r
-set SERVER_PORT_TASK5=8090
-C:\app\Java\jdk-17.0.8\bin\java -jar task5-0.0.1-SNAPSHOT.jar  > log.txt
